ffcc3706d91febebca2b4c6af8ae814712ad6b53
Mahan-Sharifi <64166204+Mahan-Sharifi@users.noreply.github.com>
2020-05-05 21:10:25 +0430

    Update Sale.java

--- HW5-1/src/com/company/Sale.java
+++ HW5-1/src/com/company/Sale.java
@@ -1,4 +1,41 @@
 package com.company;
 
 public class Sale {
+  private String ProductName;
+  private String ProductPrice;
+  private String ProductWeight;
+  
+  public String getProductName(){
+    return ProductName;
+}
+  public void setProductName(String productName)throws SaleExeption{
+    if(productName.isEmpty){
+      throw new SaleExeption("Didn't enter your ProductName");
+    }
+    else
+      ProductName=productName;
+  }
+  
+  public String getProductPrice(){
+    return ProductPrice;
+  public void setProductPrice(String productPrice)throws SaleExeption{
+    if(productPrice.isEmpty){
+      throw new SaleExeption("Didn't enter your ProductPrice");
+    }
+    else
+      ProductPrice=productPrice;
+  }
+    public String getProductWeight(){
+      return ProductWeight;
+    public void setProductWeight(String productWeight)throws SaleExeption{
+      if(productWeight.isEmpty){
+        throw new SaleExeption("Didn't enter your ProductWeight");
+      }
+      else
+        ProductPrice=productprice;
+    }
+      public void Accepting(){
+        System.out.println("The Saling was successful");
+        system.out.println("Good Luck");
+    
 }
